91792d17d141c797263df527121868d855f193fb
DhruvMitna19 <dhruvmitna9@gmail.com>
2021-09-30 11:03:39 +0530

    added comments

--- prime-number-checker/Program.cs
+++ prime-number-checker/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// The default code.
+using System;
 
 namespace prime_number_checker {
 
@@ -8,12 +9,14 @@ namespace prime_number_checker {
 
             try {
 
+                // Getting the user input.
                 Console.Write("Enter the number you want to check to be a prime number or not: ");
                 string num = Console.ReadLine();
                 int Num = int.Parse(num);
 
                 int nonDivisibles = 0;
 
+                // Calculating the output/
                 if (Num > 1) {
 
                     for (int divisor = 2; divisor < Num; divisor ++) {
@@ -28,15 +31,18 @@ namespace prime_number_checker {
                     }
 
                     if (nonDivisibles == Num - 2) Console.WriteLine("The number is prime");
-                    
+
+                // Making sure the user has entered a positive integer.
                 } else Console.WriteLine("Please enter a positive number.");
 
+            // If the user entered a string instead of an integer, this message is printed out instead of the program crashing.
             } catch (FormatException) {
 
                 Console.WriteLine("Please enter a valid value.");
 
             }
 
+            // Preventing the program from terminating immediately.
             Console.Write("\nPress enter to exit...");
             Console.ReadLine();
 
